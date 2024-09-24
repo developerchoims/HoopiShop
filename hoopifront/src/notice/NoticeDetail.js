41e5d3e4c217d468e3d9c3309860d811bde53c50
developerchoims <developerchoims@gmail.com>
2024-09-24 21:10:58 +0900

    0924 - NoticeDetail프론트 추가

---
+++ hoopifront/src/notice/NoticeDetail.js
@@ -0,0 +1,16 @@
+const NoticeDetail = () => {
+    return(
+        <div className="notice-detail-container">
+            <div className="notice-detail-box">
+                <div className="notice-detail-title-box">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="notice-detail-content-box">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+export default NoticeDetail;
