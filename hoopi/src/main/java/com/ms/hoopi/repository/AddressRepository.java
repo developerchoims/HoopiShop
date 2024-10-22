ac8764c72c4ffd840e6af70d4ce8d51b29698ee1
developerchoims <developerchoims@gmail.com>
2024-10-22 15:20:59 +0900

    10/22 - 주소 삭제 매개변수 타입 변경

--- hoopi/src/main/java/com/ms/hoopi/repository/AddressRepository.java
+++ hoopi/src/main/java/com/ms/hoopi/repository/AddressRepository.java
@@ -2,6 +2,7 @@ package com.ms.hoopi.repository;
 
 import com.ms.hoopi.model.entity.Address;
 import com.ms.hoopi.order.model.dto.OrderResponseDto;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,6 +11,7 @@ public interface AddressRepository extends CrudRepository<Address, Long> {
     @Query("SELECT a FROM Address a WHERE a.addressCode = :addressCode")
     Address findByAddressCode(String addressCode);
 
+    @Modifying
     @Query("DELETE FROM Address a WHERE a.addressCode = :addressCode")
     void deleteByAddressCode(String addressCode);
 }
